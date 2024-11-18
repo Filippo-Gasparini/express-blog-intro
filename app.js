fb2d6bfd32bd3a8c975dd16f9c792aefc21958c6
Filippo Gasparini <filippogaspa02@gmail.com>
2024-11-18 15:50:27 +0100

    ADD: express

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-console.log('express')
-
+const express = require('express')
+const app = express()
+const port = 3000
 //app.get('/', (req, res) => {
  //   console.log(req.query)
    // console.log(res)
 //}) console.log(res)
+
